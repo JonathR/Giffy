49a3d7f0693054c616574affb4740a04015c68d5
Jonathan Rubio <rubiojonathan09@gmail.com>
2021-02-01 21:36:55 -0500

    panga gifs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const apiURL =
+  'https://api.giphy.com/v1/gifs/search?api_key=d8JABKp74yCPgprniYfLrm6H4IO206z9&q=panda&limit=10&offset=0&rating=g&lang=en';
+
 function App() {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(function () {
+    fetch(apiURL)
+      .then((res) => res.json())
+      .then((response) => {
+        const { data = [] } = response;
+        if (Array.isArray(data)) {
+          const gifs = data.map((image) => image.images.downsized_medium.url);
+          setGifs(gifs);
+        }
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        {gifs.map((singleGif) => (
+          <img src={singleGif} />
+        ))}
+      </section>
     </div>
   );
 }
