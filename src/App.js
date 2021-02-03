feb87c5624654008f8b6179fc55e3f7ac47d549e
Jonathan Rubio <rubiojonathan09@gmail.com>
2021-02-02 21:59:06 -0500

    used key

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-
-const apiURL =
-  'https://api.giphy.com/v1/gifs/search?api_key=d8JABKp74yCPgprniYfLrm6H4IO206z9&q=panda&limit=10&offset=0&rating=g&lang=en';
+import Gif from './components/Gif';
+import getGifs from './services/getGifs';
 
 function App() {
   const [gifs, setGifs] = useState([]);
 
   useEffect(function () {
-    fetch(apiURL)
-      .then((res) => res.json())
-      .then((response) => {
-        const { data = [] } = response;
-        if (Array.isArray(data)) {
-          const gifs = data.map((image) => image.images.downsized_medium.url);
-          setGifs(gifs);
-        }
-      });
+    getGifs({ keyword: 'programming' }).then((gifs) => setGifs(gifs));
   }, []);
 
   return (
     <div className="App">
       <section className="App-content">
-        {gifs.map((singleGif) => (
-          <img src={singleGif} />
+        {gifs.map(({ id, title, url }) => (
+          <Gif id={id} key={id} title={title} url={url} />
         ))}
       </section>
     </div>
