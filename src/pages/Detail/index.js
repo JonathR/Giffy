0022af95a05c8e4b59142fcc6d7707cc5f6c08bb
Jonathan Rubio <rubiojonathan09@gmail.com>
2021-02-11 23:07:31 -0500

    useglobalgif

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Gif from '../../components/Gif/Gif';
-import GifsContext from '../../context/GifsContext';
+
+import useGlobalGifs from '../../hooks/useGlobalGifs';
 
 export default function Detail({ params }) {
-  const { gifs } = useContext(GifsContext);
+  const gifs = useGlobalGifs();
 
   const gif = gifs.find((singleGif) => singleGif.id === params.id);
 
