f0e911bce4e5a0dcc131649359cf02ee1352183c
Jonathan Rubio <rubiojonathan09@gmail.com>
2021-02-21 23:25:58 -0500

    Infinite Scroll

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import Spinner from 'components/Spinner';
 import ListOfGifs from 'components/ListOfGifs/ListOfGifs';
 import { useGifs } from 'hooks/useGifs';
+import useNearScreen from 'hooks/useNearScreen';
+import debounce from 'just-debounce-it';
 
 export default function SearchResults({ params }) {
   const { keyword } = params;
   const { loading, gifs, setPage } = useGifs({ keyword });
+  const externalRef = useRef();
+  const { isNearScreen } = useNearScreen({
+    externalRef: loading ? null : externalRef,
+    once: false,
+  });
 
-  const handleNextPage = () => setPage((prevPage) => prevPage + 1);
+  const debounceHandleNextPage = useCallback(
+    debounce(() => setPage((prevPage) => prevPage + 1), 200),
+    []
+  );
+
+  useEffect(
+    function () {
+      if (isNearScreen) debounceHandleNextPage();
+    },
+    [debounceHandleNextPage, isNearScreen]
+  );
 
   return (
     <>
@@ -17,10 +34,9 @@ export default function SearchResults({ params }) {
         <>
           <h3 className="App-title">{decodeURI(keyword)}</h3>
           <ListOfGifs gifs={gifs} />
+          <div id="visor" ref={externalRef}></div>
         </>
       )}
-      <br />
-      <button onClick={handleNextPage}>Get next page</button>
     </>
   );
 }
