bd7db883b0b546286a5bc462f16b5b8497923da8
Jonathan Rubio <rubiojonathan09@gmail.com>
2021-02-23 22:02:48 -0500

    SearchForm-Component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,37 +1,25 @@
-import React, { useState } from 'react';
+import React, { useCallback } from 'react';
 import { useLocation } from 'wouter';
-
 import ListOfGifs from 'components/ListOfGifs/ListOfGifs';
 import { useGifs } from 'hooks/useGifs';
 import TrendingSearches from 'components/TrendingSearches';
+import SearchForm from 'components/SearchForm';
 
 export default function Home() {
-  const [keyword, setKeyword] = useState('');
   const [path, pushLocation] = useLocation();
-  const { gifs } = useGifs();
+  const { loading, gifs } = useGifs();
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    //navegar a otra ruta
-    pushLocation(`/search/${keyword}`);
-  };
-
-  const handleChange = (evt) => {
-    setKeyword(evt.target.value);
-  };
+  const handleSubmit = useCallback(
+    ({ keyword }) => {
+      //navegar a otra ruta
+      pushLocation(`/search/${keyword}`);
+    },
+    [pushLocation]
+  );
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <button>Buscar</button>
-        <input
-          placeholder="Search a git here..."
-          onChange={handleChange}
-          type="text"
-          value={keyword}
-        />
-      </form>
-
+      <SearchForm onSubmit={handleSubmit} />
       <div className="App-main">
         <div className="App-results">
           <h3 className="App-title">Última Busqueda </h3>
